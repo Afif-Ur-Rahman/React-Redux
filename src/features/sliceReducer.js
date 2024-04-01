30c09d6daa7d82ccf9452c921271194b4da3d91d
Afif-Ur-Rahman <afifurrahman444@gmail.com>
2024-04-01 18:57:07 +0500

    Initial Commit

---
+++ src/features/sliceReducer.js
@@ -0,0 +1,21 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+const todoSlice = createSlice({
+  name: "Todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+        state.todos.filter()
+    },
+  },
+});
